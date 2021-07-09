c4286f9cf4bbe9bf4711c3f50001d79cea1ebdd2
mariusz.wojcik <mariusz.wojcik@asseco.pl>
2021-07-09 06:48:28 +0200

    micrometer

--- src/main/java/com/filichkin/blog/reactive/Application.java
+++ src/main/java/com/filichkin/blog/reactive/Application.java
@@ -1,10 +1,18 @@
 package com.filichkin.blog.reactive;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
+
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+		return registry -> registry.config().commonTags("application", "MWO-METRICS-SPRING-BOOT-APP");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
